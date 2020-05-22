5f69be2207ad44be5548cdf6f17d9e87a8b8d2df
Erucolindo <erucolindo96@gmail.com>
2020-05-22 21:06:42 +0200

    Add WorldCasesInfo returned as GeoJson format

--- src/main/java/com/covis/api/country/CountryCovidInformationResource.java
+++ src/main/java/com/covis/api/country/CountryCovidInformationResource.java
@@ -4,6 +4,7 @@ package com.covis.api.country;
 import com.covis.api.country.dto.CountryPopulationDto;
 import com.covis.api.country.dto.CovidDailyCasesDto;
 import com.covis.api.covid.CovidCasesType;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,19 +17,19 @@ import java.util.Date;
 @RequestMapping(value = "/api/countries")
 public interface CountryCovidInformationResource {
 
-    @RequestMapping(value = "/{country-slug}/epidemy-info", method = RequestMethod.GET)
+    @RequestMapping(value = "/{country-slug}/epidemy-info", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<CovidDailyCasesDto> listEpidemyDataInCountry(@RequestParam("from") @DateTimeFormat(pattern="yyyy-MM-dd") Date from,
                                                       @RequestParam("to") @DateTimeFormat(pattern="yyyy-MM-dd") Date to,
                                                       @PathVariable("country-slug") String countrySlug,
                                                       @RequestParam("type") CovidCasesType type);
 
-    @RequestMapping(value = "/{country-slug}/epidemy-forecast", method = RequestMethod.GET)
+    @RequestMapping(value = "/{country-slug}/epidemy-forecast", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public List<CovidDailyCasesDto> listEpidemyForecastInCountry(@RequestParam("from") @DateTimeFormat(pattern="yyyy-MM-dd") Date from,
                                                           @RequestParam("to") @DateTimeFormat(pattern="yyyy-MM-dd") Date to,
                                                           @PathVariable("country-slug") String countrySlug,
                                                           @RequestParam("type") CovidCasesType type);
 
-    @RequestMapping(value = "/{country-slug}/population", method = RequestMethod.GET)
+    @RequestMapping(value = "/{country-slug}/population", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
     public CountryPopulationDto getCountryPopulation(@RequestParam("when") @DateTimeFormat(pattern="yyyy-MM-dd") Date dateWhen,
                                               @PathVariable("country-slug") String countrySlug);
 
--- src/main/java/com/covis/api/world/WorldCovidInformationResource.java
+++ src/main/java/com/covis/api/world/WorldCovidInformationResource.java
@@ -1,24 +1,22 @@
 package com.covis.api.world;
 
 import com.covis.api.covid.CovidCasesType;
-import com.covis.api.dto.Dto;
-import com.covis.api.world.dto.CountryCovidInfoDto;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.stereotype.Controller;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.Date;
-import java.util.List;
 
 
 @RequestMapping(value = "/api/world")
 public interface WorldCovidInformationResource {
 
-    @RequestMapping(value = "/epidemy-info", method = RequestMethod.GET)
-    public List<CountryCovidInfoDto> listWorldEpidemyInfo(@RequestParam("date") @DateTimeFormat(pattern="yyyy-MM-dd") Date date,
+    @RequestMapping(value = "/epidemy-info", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
+    public String listWorldEpidemyInfoAsGeoJson(@RequestParam("date") @DateTimeFormat(pattern="yyyy-MM-dd") Date date,
                                                      @RequestParam("type") CovidCasesType type);
 
 
